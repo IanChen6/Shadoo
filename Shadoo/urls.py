a300c757814ed68fab80c754f80f0483ef0ef6d1
IanChen6 <30455192+IanChen6@users.noreply.github.com>
2017-10-10 22:55:08 +0800

    Django
    Django

--- Shadoo/urls.py
+++ Shadoo/urls.py
@@ -20,4 +20,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="index.html"),name="index")#1.配置首页,2.html的名称 3.为url起名
+    url(r'^suggest/$', TemplateView.as_view(template_name="index.html"),name="index")
 ]
--- search/views.py
+++ search/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+
 
 # Create your views here.
+class SearchSuggest(View):
+    def get(self,request):
+        key_words = request.GET.get("s",'')
+        re_datas=[]
+        if key_words:
+
